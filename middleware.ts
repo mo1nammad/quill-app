c4aa406ffbbb3ed61e80ea759609bdb92058e7b8
MammadProMax <mhaagrm2@gmail.com>
2023-11-28 03:14:28 +0330

    add middleware protect route

---
+++ middleware.ts
@@ -0,0 +1,9 @@
+import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
+
+export const config = {
+   matcher: ["/dashboard/:path*", "/auth-callback"],
+};
+
+export default function middleware(req: any) {
+   return withAuth(req);
+}
