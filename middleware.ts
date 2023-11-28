81702cc5431aaa782a8228b89b235a36a3ad8536
MammadProMax <mhaagrm2@gmail.com>
2023-11-28 03:32:29 +0330

    testing middleware

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,7 @@
 import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
 
 export const config = {
-   matcher: ["/dashboard/:path*", "/auth-callback"],
+   matcher: [],
 };
 
 export default function middleware(req: any) {
