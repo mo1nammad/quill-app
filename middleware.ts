ceaf8e645ecdc0388e70739d794f67f80a72b7a4
MammadProMax <mhaagrm2@gmail.com>
2023-11-28 20:21:55 +0330

    delete middleware

--- middleware.ts
+++
@@ -1,9 +0,0 @@
-import { withAuth } from "@kinde-oss/kinde-auth-nextjs/middleware";
-
-export const config = {
-   matcher: [],
-};
-
-export default function middleware(req: any) {
-   return withAuth(req);
-}
